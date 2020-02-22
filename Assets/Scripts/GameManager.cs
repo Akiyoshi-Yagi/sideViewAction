767b7890f21d7c51ef613efef20a9526dae80c2d
Akiyoshi-Yagi <akiyoshimerry@yahoo.co.jp>
2020-02-23 01:17:29 +0900

    リスタートの対応

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject GameOverText;
     [SerializeField] GameObject GameClearText;
+
     public void GameOver()
     {
         GameOverText.SetActive(true);
+        Invoke("RestartScene", 1.5f);
 
     }
     public void GameClear()
     {
         GameClearText.SetActive(true);
+        Invoke("RestartScene", 1.5f);
+    }
 
+    void RestartScene()
+    {
+        //現在のシーンを取得する
+        //現在のシーンをスタートさせる
+        Scene thisScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(thisScene.name);
     }
 
 
